5dedf98f5fcb0d61b34c28f08cfddc2158ef7f9a
VenkateshMogili <mogilivenkatesh3@gmail.com>
2020-09-02 13:39:07 +0530

    build added

---
+++ build/precache-manifest.864dc1c38dc712016a844ba7d7e4e13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c960c48a84eec1d5a7877e0b00551ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf544f227c1feed2872",
+    "url": "/static/css/main.6cb7e2e7.chunk.css"
+  },
+  {
+    "revision": "f3b3b5177488c9d44f55",
+    "url": "/static/js/2.1bdaa4b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1bdaa4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf544f227c1feed2872",
+    "url": "/static/js/main.d12b7639.chunk.js"
+  },
+  {
+    "revision": "2a7164193d4e0f707882",
+    "url": "/static/js/runtime-main.3b724e73.js"
+  }
+]);
